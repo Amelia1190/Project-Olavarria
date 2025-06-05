c83868008225179a7bcd39efbed7da2f92304356
Amelia1190 <lurbetamelialibertad@gmail.com>
2025-06-05 20:47:49 -0300

    Agregaciones

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,70 @@
 import { Injectable } from '@nestjs/common';
+import { title } from 'process';
+
+export interface iTrack {
+  id : number,
+  title: string,
+  duration: number,
+  artist : string,
+}
+
+
+export const tracks: iTrack[] = [ 
+  {
+    "id": 1,
+    "title": "Cancion 1",
+    "duration": 120,
+    "artist": "interprete 1"
+  },
+  {
+    "id": 2,
+    "title": "Cancion 2",
+    "duration": 125,
+    "artist": "interprete 1"
+  },
+  {
+    "id": 3,
+    "title": "Cancion 3",
+    "duration": 112,
+    "artist": "interprete 2"
+  },
+  {
+    "id": 4,
+    "title": "Cancion 4",
+    "duration": 210,
+    "artist": "interprete 1"
+  },
+  {
+    "id": 5,
+    "title": "Cancion 5",
+    "duration": 220,
+    "artist": "interprete 3"
+  },
+  {
+    "id": 6,
+    "title": "Cancion 6",
+    "duration": 180,
+    "artist": "interprete 3"
+  },
+  {
+    "id": 7,
+    "title": "Cancion 7",
+    "duration": 130,
+    "artist": "interprete 3"
+  },
+  {
+    "id": 8,
+    "title": "Cancion 8",
+    "duration": 150,
+    "artist": "interprete 1"
+  }
+]
+
+
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello Milo!';
+  getTracks():iTrack[] {
+    return tracks;
   }
 }
